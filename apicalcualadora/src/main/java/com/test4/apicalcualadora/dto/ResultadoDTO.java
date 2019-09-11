4761388e2da4621bc4779cd7b461d5eb782cd1bb
Baldomero <baldomeroc@gmail.com>
2019-09-11 20:38:26 +0200

    Mejora Servico, inclusión de Moedlo y DTO

---
+++ apicalcualadora/src/main/java/com/test4/apicalcualadora/dto/ResultadoDTO.java
@@ -0,0 +1,19 @@
+package com.test4.apicalcualadora.dto;
+
+public class ResultadoDTO {
+
+	private Float resultado;
+
+	public ResultadoDTO() {
+
+	}
+
+	public Float getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Float resultado) {
+		this.resultado = resultado;
+	}
+
+}
---
+++ apicalcualadora/src/main/java/com/test4/apicalcualadora/modelo/Resultado.java
@@ -0,0 +1,19 @@
+package com.test4.apicalcualadora.modelo;
+
+public class Resultado {
+
+	private Float resultado;
+
+	public Resultado() {
+
+	}
+
+	public Float getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Float resultado) {
+		this.resultado = resultado;
+	}
+
+}
